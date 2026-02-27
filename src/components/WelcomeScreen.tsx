cc9efd165619d89fdf33292f3736beeb338997d2
Jayden Tan <jaydentan@ucsb.edu>
2026-02-26 23:27:40 -0800

    fix(ui): remove badge on open file button

--- src/components/WelcomeScreen.tsx
+++ src/components/WelcomeScreen.tsx
@@ -42,7 +42,6 @@ export function WelcomeScreen({
               <path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"/>
             </svg>
             Open File
-            <span className="welcome-btn-badge">.tex</span>
           </button>
           <button
             type="button"
